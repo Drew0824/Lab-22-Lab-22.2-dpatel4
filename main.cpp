6896f79f7fe5320d9026593cf8dce335b623aecf
dpatel4 <pateldhruvil90@yahoo.com>
2021-02-16 15:00:46 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/*  Initializing arrays
+Instructions: Add code to display the value of each element from the numbers array.
+Then, add another array named myArray that is initialized with the following values:
+8.5, 9.4, 3.5, 3.6, 9.5. Also add code to display all the elements from the array myArray.*/
+
+
+
+int main() {
+
+double numbers[3] = {2.0, 5.9, 4.3};
+
+cout << numbers[0] << endl;
+cout << numbers[1] << endl;
+cout << numbers[2] << endl;
+
+double myArray [5] = {8.5, 9.4, 3.5, 3.6, 9.5};
+
+cout << myArray[0] << endl;
+cout << myArray[1] << endl;
+cout << myArray[2] << endl;
+cout << myArray[3] << endl;
+cout << myArray[4] << endl;
+
+
+}
